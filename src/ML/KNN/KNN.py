3d4b5d6b31683ab1a6463295b87c3c63d0afcba5
zhaokun00 <825537475@qq.com>
2018-11-29 23:09:28 +0800

    feature:"添加了交叉验证的示例"

--- src/ML/KNN/KNN.py
+++ src/ML/KNN/KNN.py
@@ -64,6 +64,33 @@ def knn():
     print("精确率与召回率:",classification_report(y_test,result,target_names=targetName))
 
 # 交叉验证与网格搜索
+'''
+交叉验证与网格搜索的目的:选取最优的超参数
+交叉验证:
+    将拿到的数据分为训练和验证集.例如将数据分为5份,其中一份作为验证集,然后经过5次的测试(假如有1个超参数,超参数有
+    2个取值,则要进行5*2次测试,获取最终的2个平均值,假如有2个超参数a、b,其中a有n种取值,b有m种取值,则要进行5*n*m次测试,
+    取n*m种结果,从中选取最优的一组超参数),每次都更换验证集,即得到5组模型的结果,取平均值最为最终的结果,又称为5折交叉验证
+    
+超参数搜索-网格搜索
+    通常情况下,有很多参数都是需要手动指定的,这种叫做超参数,但是手动过程繁杂,所以需要对模型预设几种超参数组合,每组超参数
+    都采用交叉验证来进行评估,最后选出最优参数组合建立模型
+    
+    
+def __init__(self, estimator, param_grid, scoring=None, fit_params=None,
+         n_jobs=1, iid=True, refit=True, cv=None, verbose=0,
+         pre_dispatch='2*n_jobs', error_score='raise',
+         return_train_score=True):
+    
+    参数:             
+    estimator:估计器对象
+    param_grid:估计器参数,为map类型
+    cv:指定几折交叉验证
+    
+    方法:
+    fit:训练数据
+    score:准确率
+    
+'''
 def knnCross():
     x_train = [[101, 10], [99, 5], [98, 2], [3, 104], [2, 100], [1, 81]]
 
@@ -91,12 +118,15 @@ def knnCross():
 
     search.fit(x_train, y_train)
 
-    result = search.predict(x_test)
+    # result = search.predict(x_test)
+    #
+    # print(result)
 
-    print(result)
-
+    # 交叉验证中最好的估计器模型
     print(search.best_estimator_)
+    # 交叉验证中最好的一组参数
     print(search.best_params_)
+    # 在交叉验证中测试最好的结果
     print(search.best_score_)
     print(search.cv_results_)
 
